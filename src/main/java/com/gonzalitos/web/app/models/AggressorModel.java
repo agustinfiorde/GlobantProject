ad394128966dbb8250601cdf4ce73ff72454155a
Jesus Rodriguez <jesusn1999@gmail.com>
2020-05-30 12:24:26 -0300

    Commit: aggressor model

--- src/main/java/com/gonzalitos/web/app/models/AggressorModel.java
+++ src/main/java/com/gonzalitos/web/app/models/AggressorModel.java
@@ -1,5 +1,26 @@
 package com.gonzalitos.web.app.models;
 
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
 public class AggressorModel {
 
+	private String id;
+	
+	private String name;
+	
+	private String secondName;
+	
+	private String lastName;
+	
+	private String dni;
+	
+	private Date registered;
+	
+	private Date edited;
+	
+	private Date remove;
+	
 }
