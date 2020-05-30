8a4ff46ffcd10ace215dbfc0338f0145cd96943c
Agustin Fiordelisi <agulirio@yahoo.com.ar>
2020-05-30 11:21:06 -0300

    Commit inicial

---
+++ src/main/java/com/gonzalitos/web/app/GonzalitosApplication.java
@@ -0,0 +1,13 @@
+package com.gonzalitos.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonzalitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonzalitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonzalitos/web/app/GonzalitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzalitos.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonzalitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
