06f3ee45a3b8fdd998d7c944b5b8e777a96cd372
Tomas Mazuran <tomy09201@gmail.com>
2020-05-30 21:10:08 -0300

    Commit:  relationship
    falta hacer bien el listado

--- src/main/java/com/gonzalitos/web/app/controllers/RelationshipController.java
+++ src/main/java/com/gonzalitos/web/app/controllers/RelationshipController.java
@@ -1,5 +1,33 @@
 package com.gonzalitos.web.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gonzalitos.web.app.entities.Relationship;
+import com.gonzalitos.web.app.services.RelationshipService;
+
+@Controller
+@RequestMapping("/relationship")
 public class RelationshipController {
 
+	@Autowired
+	private RelationshipService relationshipService;
+	
+	@GetMapping("/list")
+	public String list(ModelMap modelo) {
+//		Relationship relationship = relationshipService.findAll();
+//		modelo.put("relationships", relationship);
+		return "relationship-list.html";
+	}
+	
+	@GetMapping("/form")
+	public String form(ModelMap modelo) {
+		Relationship relationship = new Relationship();
+		modelo.put("relationship", relationship);
+		return "relationship-form.html";
+	}
+	
 }
